8f29072db35ada57d9a123ad7f986e88872cd755
Nico Hagelberg <nico.hagelberg@gmail.com>
2016-10-10 18:47:50 +0300

    Kommentoitu pois SpringApplicationBuilder kun .war buildattu

--- src/main/java/me/nicou/tuntikirjaus/TuntikirjausApplication.java
+++ src/main/java/me/nicou/tuntikirjaus/TuntikirjausApplication.java
@@ -2,10 +2,22 @@ package me.nicou.tuntikirjaus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TuntikirjausApplication {
+public class TuntikirjausApplication extends SpringBootServletInitializer {
 
+	/*
+	 * Tää pitää ottaa pois kommenteista
+	 * sillon kun buildtaan .war tomcattia varten
+	 * 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TuntikirjausApplication.class);
+	}
+	*/
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TuntikirjausApplication.class, args);
 	}
