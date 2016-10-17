1aa84b92a60dc0c924a40a70b6ea205cd5f1f408
Nico Hagelberg <nico.hagelberg@gmail.com>
2016-10-17 10:47:29 +0300

    Session tallennus samaan SQL kantaan kun muu data, Redis pois

---
+++ src/main/java/me/nicou/tuntikirjaus/HttpSessionConfig.java
@@ -0,0 +1,11 @@
+package me.nicou.tuntikirjaus;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
+
+@Configuration
+@EnableJdbcHttpSession
+public class HttpSessionConfig {
+
+
+}
--- src/main/java/me/nicou/tuntikirjaus/SessionInitializer.java
+++
@@ -1,7 +0,0 @@
-package me.nicou.tuntikirjaus;
-
-import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
-
-public class SessionInitializer extends AbstractHttpSessionApplicationInitializer {
-
-}
